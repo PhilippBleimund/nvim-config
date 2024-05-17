2a2adfcc3ac0bc3986a0211fb1c5f3c52ce35a60
Philipp Bleimund <bleimund.philipp@gmail.com>
2024-05-18 00:58:09 +0200

    some changes

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,4 +1,4 @@
-local configs = require("nvchad.configs.lspconfig")
+local configs = require "nvchad.configs.lspconfig"
 
 local on_attach = configs.on_attach
 local on_init = configs.on_init
@@ -19,30 +19,7 @@ for _, lsp in ipairs(servers_change) do
   }
 end
 
--- Find python path if switch on virtual env
-local function get_python_path(workspace)
-  -- Use activated virtualenv.
-  if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
-  end
-
-  -- Find and use virtualenv via poetry in workspace directory.
-  local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
-  if match ~= "" then
-    local venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
-    return path.join(venv, "bin", "python")
-  end
-
-  -- Fallback to system Python.
-  return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
-end
-
 lspconfig.pylsp.setup {
-  on_init = function(client)
-    local pythonPath = get_python_path(client.config.root_dir)
-    client.config.settings.python.pythonPath = pythonPath
-    print(pythonPath)
-  end,
   on_attach = on_attach,
   settings = {
     pylsp = {
