9528863e4629504e22078d51bb7edb43a6c7710f
Philipp Bleimund <bleimund.philipp@gmail.com>
2024-07-19 23:00:34 +0200

    added custom arguments to black

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -7,9 +7,13 @@ local sources = {
     },
   },
   null_ls.builtins.formatting.stylua,
-  null_ls.builtins.formatting.black,
-  null_ls.builtins.formatting.isort,
+  null_ls.builtins.formatting.black.with {
+    extra_args = {
+      "--line-length=120",
+    },
+  },
 }
+
 null_ls.setup {
   sources = sources,
   debug = true,
