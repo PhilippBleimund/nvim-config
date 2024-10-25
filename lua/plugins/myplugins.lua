acf2de6ad9aee38a088098d23120a54d81f7fbf0
Philipp Bleimund <bleimund.philipp@gmail.com>
2024-10-25 12:26:33 +0200

    added new plugin

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -145,5 +145,29 @@ local plugins = {
       },
     },
   },
+  {
+    "stevearc/aerial.nvim",
+    lazy = false,
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("aerial").setup {
+        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+        on_attach = function(bufnr)
+          -- Jump forwards/backwards with '{' and '}'
+          vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+          vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+        end,
+      }
+      -- You probably also want to set a keymap to toggle aerial
+      vim.keymap.set("n", "<leader>at", "<cmd>AerialToggle!<CR>")
+      vim.keymap.set("n", "<leader>aa", "<cmd>AerialOpen<CR>")
+      vim.keymap.set("n", "<leader>ac", "<cmd>AerialClose<CR>")
+    end,
+  },
 }
 return plugins
