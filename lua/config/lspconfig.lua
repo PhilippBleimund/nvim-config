6bbf4f50b2aa728074642c47058e2d7e889d2276
Philipp Bleimund <bleimund.philipp@gmail.com>
2025-08-22 23:55:02 +0200

    updated

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -25,12 +25,12 @@ local function setup_servers()
 end
 
 local function setup_diags()
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  vim.diagnostic.config {
     virtual_text = false,
     signs = true,
     update_in_insert = false,
     underline = true,
-  })
+  }
 end
 
 setup_servers()
